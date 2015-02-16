8ec649fec8c0ed76a222e63cb0d7035d04088e60
Wim Bonekamp <wim@wibosa.nl>
2015-02-16 15:37:33 +0100

    14 vultr regions in table TODO check intergar mapping

--- lib/fog/vultr/requests/compute/list_regions.rb
+++ lib/fog/vultr/requests/compute/list_regions.rb
@@ -18,12 +18,20 @@ module Fog
           response.body = {
             "status" => "OK",
             "regions"  => [
-              { "id" => 1, "name" => "New York 1" },
-              { "id" => 2, "name" => "Amsterdam 1" },
-              { "id" => 3, "name" => "San Francisco 1" },
-              { "id" => 4, "name" => "New York 2" },
-              { "id" => 5, "name" => "Amsterdam 2" },
-              { "id" => 6, "name" => "Singapore 1" }
+                { "id" => 1, "name" => "Chicago, Illinois " },
+                { "id" => 2, "name" => "New York / New Jersey " },
+                { "id" => 3, "name" => "Dallas, Texas " },
+                { "id" => 4, "name" => "Atlanta, Georgia " },
+                { "id" => 5, "name" => "Miami, Florida " },
+                { "id" => 6, "name" => "Silicon Valley, California " },
+                { "id" => 7, "name" => "Los Angeles, California " },
+                { "id" => 8, "name" => "Seattle, Washington " },
+                { "id" => 9, "name" => "(AU) Sydney, Australia " },
+                { "id" => 10, "name" => "(Asia) Tokyo, Japan " },
+                { "id" => 11, "name" => "(EU) Amsterdam, NL " },
+                { "id" => 12, "name" => "(EU) Frankfurt, DE " },
+                { "id" => 13, "name" => "(EU) London, UK " },
+                { "id" => 14, "name" => "(EU) Paris, France " }
             ]
           }
           response
