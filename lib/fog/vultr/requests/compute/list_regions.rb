52d4bf378ec2d28ca42c848817bfc4b9c784f146
Wim Bonekamp <wim@wibosa.nl>
2015-02-16 16:03:45 +0100

    check regio ID map with api response

--- lib/fog/vultr/requests/compute/list_regions.rb
+++ lib/fog/vultr/requests/compute/list_regions.rb
@@ -18,20 +18,20 @@ module Fog
           response.body = {
             "status" => "OK",
             "regions"  => [
-                { "id" => 1, "name" => "Chicago, Illinois " },
-                { "id" => 2, "name" => "New York / New Jersey " },
+                { "id" => 1, "name" => "New York / New Jersey " },
+                { "id" => 2, "name" => "Chicago, Illinois " },
                 { "id" => 3, "name" => "Dallas, Texas " },
-                { "id" => 4, "name" => "Atlanta, Georgia " },
-                { "id" => 5, "name" => "Miami, Florida " },
-                { "id" => 6, "name" => "Silicon Valley, California " },
-                { "id" => 7, "name" => "Los Angeles, California " },
-                { "id" => 8, "name" => "Seattle, Washington " },
-                { "id" => 9, "name" => "(AU) Sydney, Australia " },
-                { "id" => 10, "name" => "(Asia) Tokyo, Japan " },
-                { "id" => 11, "name" => "(EU) Amsterdam, NL " },
-                { "id" => 12, "name" => "(EU) Frankfurt, DE " },
-                { "id" => 13, "name" => "(EU) London, UK " },
-                { "id" => 14, "name" => "(EU) Paris, France " }
+                { "id" => 4, "name" => "Seattle, Washington " },
+                { "id" => 5, "name" => "Los Angeles, California " },
+                { "id" => 6, "name" => "Atlanta, Georgia " },
+                { "id" => 7, "name" => "(EU) Amsterdam, NL " },
+                { "id" => 8, "name" => "(EU) London, UK " },
+                { "id" => 9, "name" => "(EU) Frankfurt, DE " },
+                { "id" => 12, "name" => "Silicon Valley, California " },
+                { "id" => 19, "name" => "(AU) Sydney, Australia " },
+                { "id" => 24, "name" => "(EU) Paris, France " },
+                { "id" => 25, "name" => "(Asia) Tokyo, Japan " },
+                { "id" => 39, "name" => "Miami, Florida " }
             ]
           }
           response
